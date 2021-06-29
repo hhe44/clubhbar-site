0c1f94542d39b54636c2f321cd57c2e99fd58705
unknown <hhe@mailfence.com>
2021-06-29 17:39:12 -0400

    navbar css class name tweaks

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,26 +1,27 @@
-import styles from "./Navbar.module.scss";
 import Image from "next/image";
+import styles from "./Navbar.module.scss";
+import logo from "../public/images/clubhbar-logo.svg";
 
 // Send GET request to coingecko for HBAR prices in future
 
 const Navbar = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.left} style={{ position: 'relative', width: '11rem', height: '8rem' }}>
+    <div className={styles['nav-container']}>
+      <div style={{ position: 'relative', width: '11rem', height: '8rem' }}>
         <Image
-          src="/images/clubhbar-logo.svg"
+          src={logo}
           layout='fill'
           objectFit="cover"
           alt="ClubHbar Logo"
         />
       </div>
-      <ul className={styles.center}>
+      <ul className={styles['nav-links']}>
         <li>LEARN</li>
         <li>NEWS</li>
         <li>COMMUNITY</li>
         <li>CHART</li>
       </ul>
-      <div className={styles.right}>
+      <div className={styles['nav-price']}>
         <h4>Largest Hbar Community on the Net</h4>
         <div>
           <p>Current HBAR Price: </p>
