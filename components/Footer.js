64e035d92a9dd543b3c4e0eece09f7cf4c0646ed
unknown <hhe@mailfence.com>
2021-07-01 21:13:29 -0400

    complete footer component

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,25 @@
 import styles from "./Footer.module.scss";
+// import discordIcon from "../public/images/discord-icon.svg";
+// import twitterIcon from "../public/images/twitter-icon.svg";
+// import redditIcon from "../public/images/reddit-icon.svg";
+import Image from "next/image";
 
 const Footer = () => {
   return (
+    <>
     <div className={styles.container}>
-        <ul className={styles['social-media']}></ul>
-        <p className={styles.disclaimer}></p>
+      <div>
+        <img src="/images/discord-icon.svg" alt="discord" />
+        <img src="/images/twitter-icon.svg" alt="twitter" />
+        <img src="/images/reddit-icon.svg" alt="reddit" />
+      </div>
+      <div>
+        <p className={styles.disclaimer}>
+          This is a community run site that is not affiliated with Hedera
+          Hashgraph or Swirlds <br/> (C) 2021 All Rights Reserved</p>
+      </div>
     </div>
+    </>
   );
 };
 
