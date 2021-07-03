a9aa07646c4a4174f6e047e5aa87ba69e9e379d5
unknown <hhe@mailfence.com>
2021-07-03 01:48:44 -0400

    basic posts setup w/ data fetching, dynamic paths and md -> html conversion

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,38 @@
-import styles from "../styles/Home.module.scss";
+import Link from "next/link";
+import Date from '../components/date';
+import { getSortedPostsData } from '../lib/posts';
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Footer from "../components/Footer";
+import styles from "../styles/Home.module.scss";
 
-const Home = () => {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+
+const Home = ({ allPostsData }) => {
   return (
     <>
       <Navbar isHomePage={true} />
       <Hero />
       <div className={styles.container}>
-        
+      <ul className={styles.posts}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <Date dateString={date} />
+            </li>
+          ))}
+        </ul>
       </div>
       <Footer />
     </>
