f7bb64a69f606432f58dab8ce61ce2abd587f7c1
unknown <hhe@mailfence.com>
2021-07-08 18:32:48 -0400

    update invite link

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <>
     <div className={styles.container}>
       <div>
-        <a href="https://discord.gg/XqVEP67Re9" target="_"><img src="/images/discord-icon.svg" alt="discord" /></a>
+        <a href="https://discord.gg/club-hbar" target="_"><img src="/images/discord-icon.svg" alt="discord" /></a>
         <a href="https://twitter.com/HbarWhereTheyAt" target="_"><img src="/images/twitter-icon.svg" alt="twitter" /></a>
         <img src="/images/reddit-icon.svg" alt="reddit" title="Club HBAR Reddit - Coming soon!" />
       </div>
