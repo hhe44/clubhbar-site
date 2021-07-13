0b4f78666e4fae15a052faf3445b442ae58acfdd
unknown <hhe@mailfence.com>
2021-07-13 16:35:54 -0400

    basic styling for news and community page

--- pages/community.js
+++ pages/community.js
@@ -1,10 +1,43 @@
+import Image from "next/image";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
+import background from "../public/images/clubhbar-hashgraph-bgasd.svg";
+import styles from "../styles/community.module.scss";
 
 const Community = () => {
   return (
     <>
       <Navbar />
+      <div className={styles.bgWrap}>
+          <Image
+            alt="hashgraph-bg"
+            src={background}
+            layout="fill"
+            objectFit="cover"
+            objectPosition="top"
+            quality={50}
+          />
+        </div>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.topRow}>
+            <div className={styles.memes}>
+              <h3>MEMES</h3>
+            </div>
+          </div>
+          <div className={styles.bottomRow}>
+            <div className={styles.leaderboard}>
+              <h3>LEADERBOARD</h3>
+            </div>
+            <div className={styles.games}>
+              <h3>GAMES</h3>
+            </div>
+            <div className={styles.link}>
+              <h3>LINKS</h3>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
--- pages/news.js
+++ pages/news.js
@@ -1,10 +1,43 @@
+import Image from "next/image";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
+import background from "../public/images/clubhbar-hashgraph-bgasd.svg";
+import styles from "../styles/news.module.scss";
 
 const News = () => {
   return (
     <>
       <Navbar />
+      <div className={styles.bgWrap}>
+          <Image
+            alt="hashgraph-bg"
+            src={background}
+            layout="fill"
+            objectFit="cover"
+            objectPosition="top"
+            quality={50}
+          />
+        </div>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.topRow}>
+            <div className={styles.featuredNews}>
+              <h3>FEATURED NEWS</h3>
+            </div>
+          </div>
+          <div className={styles.bottomRow}>
+            <div className={styles.announcements}>
+              <h3>ANNOUNCEMENTS</h3>
+            </div>
+            <div className={styles.tweets}>
+              <h3>TWEETS</h3>
+            </div>
+            <div className={styles.articles}>
+              <h3>ARTICLES</h3>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
