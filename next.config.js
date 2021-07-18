a9379a74d11d5bbf306cc9e9019a6861ddbb54b1
unknown <hhe@mailfence.com>
2021-07-17 23:21:37 -0400

    fixing transpilation error w/ next.js & tv-embed dependency

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
 }
+
+const withTM = require('next-transpile-modules')(['react-tradingview-embed']); // pass the modules you would like to see transpiled
+
+module.exports = withTM();
