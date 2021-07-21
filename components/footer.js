e8e7334242a2809c4a211551c36270fd918a0e50
unknown <hhe@mailfence.com>
2021-07-21 00:53:32 -0400

    use prettier on all the files

--- components/footer.js
+++ components/footer.js
@@ -3,20 +3,28 @@ import styles from "./footer.module.scss";
 const Footer = () => {
   return (
     <>
-    <div className={styles.container}>
-      <div>
-        <a href="https://discord.gg/club-hbar" target="_"><img src="/images/discord-icon.svg" alt="discord" /></a>
-        <a href="https://twitter.com/HbarWhereTheyAt" target="_"><img src="/images/twitter-icon.svg" alt="twitter" /></a>
-        <img src="/images/reddit-icon.svg" alt="reddit" title="Club HBAR Reddit - Coming soon!" />
+      <div className={styles.container}>
+        <div>
+          <a href="https://discord.gg/club-hbar" target="_">
+            <img src="/images/discord-icon.svg" alt="discord" />
+          </a>
+          <a href="https://twitter.com/HbarWhereTheyAt" target="_">
+            <img src="/images/twitter-icon.svg" alt="twitter" />
+          </a>
+          <img
+            src="/images/reddit-icon.svg"
+            alt="reddit"
+            title="Club HBAR Reddit - Coming soon!"
+          />
+        </div>
+        <div className={styles.disclaimer}>
+          <p>
+            This is a community run site that is not affiliated with Hedera
+            Hashgraph or Swirlds
+          </p>
+          <p>(C) 2021 All Rights Reserved</p>
+        </div>
       </div>
-      <div className={styles.disclaimer}>
-        <p>
-          This is a community run site that is not affiliated with Hedera
-          Hashgraph or Swirlds
-        </p>
-        <p>(C) 2021 All Rights Reserved</p>
-      </div>
-    </div>
     </>
   );
 };
--- components/twitterTimeline.js
+++ components/twitterTimeline.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { Timeline } from "react-twitter-widgets";
-import styles from "./twitterTimeline.module.scss"
+import styles from "./twitterTimeline.module.scss";
 
 const TwitterTimeline = ({ twtHandle, twtHeight }) => {
   return (
     <div className={styles.container}>
-        <Timeline
-          dataSource={{
-            sourceType: "profile",
-            screenName: twtHandle,
-          }}
-          options={{
-            theme: "dark",
-            height: twtHeight,
-            chrome: "transparent, nofooter, noscrollbar, noheader",
-            dnt: true
-          }}
-        />
+      <Timeline
+        dataSource={{
+          sourceType: "profile",
+          screenName: twtHandle,
+        }}
+        options={{
+          theme: "dark",
+          height: twtHeight,
+          chrome: "transparent, nofooter, noscrollbar, noheader",
+          dnt: true,
+        }}
+      />
     </div>
   );
 };
