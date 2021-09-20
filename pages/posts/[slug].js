a0f52d1bb4aef234070e587388a244dcba0790b4
unknown <hhe@mailfence.com>
2021-09-19 21:43:13 -0400

    properly process images w/ remark

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -2,11 +2,12 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import remark from "remark";
-import html from "remark-html";
+import remarkHtml  from "remark-html";
+import remarkUnwrapImages from 'remark-unwrap-images'
 import Date from "../../components/date";
 import Footer from "../../components/footer";
 import Navbar from "../../components/navbar";
-import sanitizeHtml from "sanitize-html";
+// import sanitizeHtml from "sanitize-html";
 import styles from "../../styles/posts.module.scss";
 
 export async function getServerSideProps() {
@@ -18,20 +19,24 @@ export async function getServerSideProps() {
 }
 
 const Post = ({ postData }) => {
+
   const { asPath } = useRouter();
   const slug = asPath.split("/").at(-1);
   const post = postData.find((post) => post.slug == slug);
   const { title, text, cover } = post;
 
   const [content, setContent] = useState({});
+  const processRawText = async () => {
+    const result = await remark()
+      .use(remarkUnwrapImages)
+      .use(remarkHtml)
+      .process(text);
+    setContent({ result });
+  };
   useEffect(() => {
-    const processRawText = async () => {
-      const result = await remark().use(html).process(text);
-      setContent({ result });
-    };
     processRawText();
   }, [text]);
-  const cleanHtml = sanitizeHtml(content.result);
+  const postHtml = content.result || "<h1>Loading...</h1>";
 
   return (
     <>
@@ -52,7 +57,7 @@ const Post = ({ postData }) => {
           />
           <div
             className={styles.postText}
-            dangerouslySetInnerHTML={{ __html: cleanHtml }}
+            dangerouslySetInnerHTML={{ __html: postHtml }}
           />
         </article>
       </div>
