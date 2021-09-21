8f9a8506d7646d8d8c1bc8d3b82632cefe7ac6f2
unknown <hhe@mailfence.com>
2021-09-20 22:19:28 -0400

    retrieve and render data from learn endpoint (strapi)

--- pages/learn.js
+++ pages/learn.js
@@ -2,15 +2,21 @@ import Image from "next/image";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import YtModal from "../components/ytModal";
-import { Tweet } from 'react-twitter-widgets'
+import { Tweet } from "react-twitter-widgets";
 import background from "../public/images/clubhbar-hashgraph-bgasd.svg";
 import styles from "../styles/learn.module.scss";
 
-// Code Optimization: Write for loop for vidlists later
-// Should loop through an array of youtube video keys to
-// create div element that wraps around h3 & ytModal
+export async function getStaticProps() {
+  const res = await fetch(`${process.env.STRAPI_URL}/learn`);
+  const learnData = await res.json();
+  return {
+    props: { learnData },
+  };
+}
 
-const Learn = () => {
+const Learn = ({ learnData }) => {
+  const { adoptionTweets, crashcourse, whyhbar } = learnData;
+
   return (
     <>
       <Navbar />
@@ -30,43 +36,39 @@ const Learn = () => {
             <div className={styles.crashCourse}>
               <h1>HEDERA HASHGRAPH CRASH COURSE</h1>
               <div className={styles.vidWrap}>
-                <YtModal vidKey={`ySXQfobVoc4`} />
-                <YtModal vidKey={`4pLFwl72Ku0`} />
-                <YtModal vidKey={`AEXfaMyZz5g`} />
+                {crashcourse.map(({ youtubeUrl }) => (
+                  <YtModal vidKey={youtubeUrl.split("=")[1]} />
+                ))}
               </div>
             </div>
           </div>
           <div className={styles.bottomRow}>
             <div className={styles.adoption}>
               <h1>ADOPTION</h1>
-              <Tweet tweetId="1401996757479956497" options={{ theme: "dark" }}/>
-              <Tweet tweetId="1402958509206089730" options={{ theme: "dark" }}/>
+              {adoptionTweets.map(({tweetUrl}) => (
+                <Tweet 
+                  tweetId={tweetUrl.split('/').splice(-1)[0]}
+                  options={{ theme: "dark" }}
+                />
+              ))}
+              {/* <Tweet
+                tweetId="1401996757479956497"
+                options={{ theme: "dark" }}
+              />
+              <Tweet
+                tweetId="1402958509206089730"
+                options={{ theme: "dark" }}
+              /> */}
             </div>
             <div className={styles.hbarShillPane}>
               <h1>WHY HBAR?</h1>
               <div className={styles.vidWrap}>
-                <div>
-                  <h3>GOVERNANCE</h3>
-                  <YtModal vidKey={`PulEjfEkWL0`} />
-                </div>
-                <div>
-                  <h3>SPEED</h3>
-                  <YtModal vidKey={`m70eiIc6J3M`} />
-                </div>
-                <div>
-                  <h3>DECENTRALIZATION</h3>
-                  <YtModal vidKey={`QTNNYeSks-s`} />
-                </div>
-                <div>
-                  <h3>SECURITY</h3>
-                  <YtModal vidKey={`pcToFASnyrc`} />
-                </div>
-                <div>
-                  <h3>STABILITY</h3>
-                  <YtModal vidKey={`mYrTBxfanPU`} />
-                </div>
-              </div>
-              <div>
+                {whyhbar.map(({ title,youtubeUrl }) => (
+                  <div>
+                    <h3>{title}</h3>
+                    <YtModal vidKey={youtubeUrl.split("=")[1]} />
+                  </div>
+                ))}
               </div>
             </div>
           </div>
